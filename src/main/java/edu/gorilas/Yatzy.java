3a7f3feb05297ccfc71d3956fa2dadad68a16ed8
b4391co <b4391co@proton.me>
2023-12-14 16:14:20 +0100

    Refactor(Yatzy): threes

--- src/main/java/edu/gorilas/Yatzy.java
+++ src/main/java/edu/gorilas/Yatzy.java
@@ -40,14 +40,11 @@ public class Yatzy {
         return sum;
     }
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
+    public static int threes(int... dice) {
+        int s = 0;    
+        for (int i : dice) {
+            if (i == 3) s += 3;
+        }
         return s;
     }
 
